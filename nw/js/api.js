7432b12750fc23752620ba8bd524227ab0fe5d8f
弱冠 <qianshan.wengqs@alibaba-inc.com>
2014-07-21 18:01:19 +0800

    增加node-webkit版

---
+++ nw/js/api.js
@@ -0,0 +1,363 @@
+var fs = require('fs');
+var os = require('os');
+var exec = require('child_process').exec;
+var platform = os.platform();
+
+var Platform = {
+    interfaces: os.networkInterfaces(),
+    isMac: platform === 'darwin',
+    isWin: platform === 'win32',
+    checkPermission: function(path, mask, callback) {
+        /*canExecute():
+        checkPermission (<path>, 1, cb);
+
+        canRead():
+        checkPermission (<path>, 4, cb);
+
+        canWrite():
+        checkPermission (<path>, 2, cb);*/
+        fs.stat(path, function(err, stats) {
+            if(err) {
+                callback && callback.call(null, err, false);
+            }else {
+                callback && callback.call(null, null, !!(mask & parseInt ((stats.mode & parseInt ("777", 8)).toString (8)[0])));
+            }
+        })
+    }
+};
+//console.log(Platform)
+
+for (var i in Platform.interfaces) {
+    //console.log(Platform.interfaces[i])
+}
+
+//Apache
+var Apache = function() {
+    this.cmd = {
+        start: Platform.isWin ? 'net start apache2.2' : 'sudo apachectl start',
+        stop: Platform.isWin ? 'net stop apache2.2' : 'sudo apachectl stop',
+        restart: Platform.isWin ? 'net restart apache2.2' : 'sudo apachectl restart'
+    };
+};
+Apache.prototype = {
+    start: function() {
+        exec(this.cmd.start, function(err, stdout, stderr) {
+
+        });
+    },
+    stop: function() {
+        exec(this.cmd.stop, function(err, stdout, stderr) {
+
+        });
+    },
+    restart: function(callback) {
+        var _this = this;
+        if (Platform.isMac) {
+            exec(this.cmd.restart, function(err, stdout, stderr) {
+                callback && callback.call(_this, err, stdout, stderr);
+                if (err) {
+                    console.log('重启失败:mac');
+                    return;
+                }
+                console.log('重启成功:mac')
+            });
+        } else {
+            //windows
+            exec(this.cmd.stop, function(err, stdout, stderr) {
+                if (err) {
+                    callback && callback.call(_this, err, stdout, stderr);
+                    console.log('停止失败');
+                    return;
+                }
+                exec(_this.cmd.start, function(err, stdout, stderr) {
+                    callback && callback.call(_this, err, stdout, stderr);
+                    if (err) {
+                        console.log('重启失败');
+                        return;
+                    }
+                    console.log('重启成功:windows');
+                })
+            })
+        }
+    }
+};
+
+//var apache = new Apache();
+//apache.restart();
+
+//HOST
+var Host = function() {
+    this.path = Platform.isWin ? 'C:/Windows/System32/drivers/etc/hosts' : '/etc/hosts';
+    //this.path = './hosts'
+};
+Host.prototype = {
+    //检查权限
+    checkPermission: function(mask, callback) {
+        Platform.checkPermission(this.path, mask, callback);
+    },
+    read: function(callback) {
+        var _this = this;
+        fs.readFile(this.path, 'utf8', function(err, data) {
+            callback && callback.call(_this, err, data);
+        });
+    },
+    write: function(content, callback) {
+        var _this = this;
+        fs.writeFile(this.path, content, function(err, data) {
+            callback && callback.call(_this, err);
+        });
+    }
+};
+
+//虚拟主机
+var Httpd = function() {
+    this.cache = {
+        data: null,
+        items: []
+    };
+    this.path = Platform.isWin ? 'D:/AppServ/Apache2.2/conf/extra/httpd-vhosts.conf' : '/etc/apache2/extra/httpd-vhosts.conf';
+    //this.path = './httpd-vhosts.conf';
+    //正则
+    this.pattern = {
+        vhost: /<VirtualHost\s+[^>]*?>[\s\S]*?<\/VirtualHost>/ig,
+        serverName: /ServerName\s+"?(.+)"?/,
+        documentRoot: /DocumentRoot\s+("?)(.+)(\1)/,
+        proxy: {
+            pass: /[^\n]*ProxyPass +.* +.*(?:\n?\r?)/ig,
+            request: /[^\n]*ProxyRequests\s+Off(?:\n?\r?)/ig,
+            proxy: /[^\n]*<Proxy\s+[^>]*?>[\s\S]*?<\/Proxy>(?:\n?\r?)/ig,
+            reverse: /[^\n]*ProxyPassReverse +.* +.*(?:\n?\r?)/ig
+        }
+    };
+};
+Httpd.prototype = {
+    init: function() {
+        var _this = this;
+        this.readFileSync();
+        /*var conf = _this.updateItem('webapp.dev', {
+            name: 'laiwang.com',
+            root: '/etc/host/',
+            proxy: [{
+                path: '/',
+                proxy: 'http://127.0.0.1:3000/'
+            }, {
+                path: '/js',
+                proxy: 'http://127.0.0.1:3000/js'
+            }]
+        });
+        console.log(conf);*/
+        //fs.writeFile('c.txt', conf);
+    },
+    //检查权限
+    checkPermission: function(mask, callback) {
+        Platform.checkPermission(this.path, mask, callback);
+    },
+    //获取配置文件
+    getData: function() {
+        //return this.cache.data || this.readFileSync();
+        return this.readFileSync();
+    },
+    //读取vhost文件
+    readFileSync: function() {
+        console.log('读取vhost文件...');
+        var _this = this;
+        var data = fs.readFileSync(this.path, 'utf8');
+        if (!data) {
+            console.log('读取vhost文件失败');
+            return data;
+        }
+        this.cache.data = data;
+        _this.cache.items = [];
+        var list = this.getList(data);
+        if (!list) {
+            console.log('没有匹配到内容, 可能是文件不对');
+            return data;
+        }
+        list.forEach(function(item) {
+            var obj = _this.getObj(item);
+            _this.cache.items.push(obj);
+        });
+        return data;
+    },
+    //读取
+    readFile: function(callback) {
+        var _this = this;
+        fs.readFile(this.path, 'utf8', function(err, data) {
+            callback && callback.call(_this, err, data);
+        });
+    },
+    //写入
+    writeFile: function(data, callback) {
+        var _this = this;
+        fs.writeFile(this.path, data, function(err, data) {
+            callback && callback.call(_this, err, data);
+        });
+    },
+    //获取列表
+    getList: function(data) {
+        return data.match(this.pattern.vhost);
+    },
+    //获取对象
+    getObj: function(data) {
+        var _this = this;
+        var name = data.match(this.pattern.serverName);
+        var root = data.match(this.pattern.documentRoot);
+        //var proxy = data.match(this.pattern.proxy.reverse);
+        //console.log(data, data.match(this.pattern.proxy.pass));
+        var proxy = {
+            pass: data.match(this.pattern.proxy.pass),
+            request: data.match(this.pattern.proxy.request),
+            proxy: data.match(this.pattern.proxy.proxy),
+            reverse: data.match(this.pattern.proxy.reverse)
+        };
+        if(proxy.pass) {
+            proxy.list = [];
+            proxy.pass.forEach(function(item) {
+                var list = item.trim().split(/\s+/);
+                proxy.list.push({
+                    dir: list[1],
+                    path: list[2]
+                });
+                
+            });
+        }
+        return {
+            name: name[1],
+            root: root[2],
+            proxy: proxy,
+            input: data
+        };
+    },
+    //获取一条数据
+    getItem: function(name) {
+        this.getData();
+        if (this.cache.items.length <= 0) {
+            console.log('尚未读取到vhost文件');
+            return;
+        }
+        var result = null;
+        this.cache.items.forEach(function(item) {
+            //console.log(item);
+            if (item.name === name) {
+                result = item;
+            }
+        });
+        return result;
+    },
+    //删除一条数据
+    removeItem: function(name) {
+        var data = this.getItem(name);
+        if(!data) {
+            console.log('没有' + name + '这个主机');
+            return;            
+        }
+        var conf = this.readFileSync().replace(data.input, '');
+        var result = fs.writeFileSync(this.path, conf);
+        conf = this.readFileSync();
+        console.log('删除主机：', result);
+        return conf;
+    },
+    //更新一个虚拟主机
+    updateItem: function(name, options, callback) {
+        var obj = this.getItem(name);
+        if (!obj) {
+            console.log('虚拟主机' + name + '不存在');
+            return;
+        }
+        var data = obj.input;
+        if (options && options.name) {
+            data = data.replace(new RegExp(obj.name, 'g'), options.name);
+        }
+        if (options && options.root) {
+            data = data.replace(new RegExp(obj.root, 'g'), options.root);
+        }
+
+        if (options && options.proxy) {
+            data = this.updateProxy(data, options.proxy, obj);
+        } else if (options && options.proxy === '') {
+            //清空代理配置
+            data = this.removeProxy(data);
+        }
+        //console.log(obj.input, data);
+        var conf = this.cache.data.replace(obj.input, data);
+        //console.log(conf);
+        this.writeFile(conf, function(error) {
+            callback && callback.call(null, error, data);
+        });
+    },
+    //删除反向代理
+    removeProxy: function(data) {
+        var text = data.replace(this.pattern.proxy.pass, '')
+            .replace(this.pattern.proxy.request, '')
+            .replace(this.pattern.proxy.proxy, '')
+            .replace(this.pattern.proxy.reverse, '');
+        return text;
+    },
+    //更新反向代理
+    updateProxy: function(data, proxy, obj) {
+        if (!proxy || proxy.length < 1) {
+            console.log('no proxy');
+            return data;
+        }
+        //已有反向代理
+        var _proxy = this.createProxy(proxy);
+        if (obj.proxy.request) {
+            //第一步清空反向代理配置
+            var text = data.replace(this.pattern.proxy.request, '##Proxy##\n');
+            text = this.removeProxy(text);
+            text = text.replace('##Proxy##\n', _proxy);
+            return text;
+        } else {
+            //console.log(data.match(/(\n).*(<\/VirtualHost>)/));
+            data = data.replace(/(\n).*(<\/VirtualHost>)/, '$1' + _proxy + '$2');
+            return data;
+        }
+    },
+    //创建代理
+    createProxy: function(proxy) {
+        var strs = ['ProxyRequests Off\n',
+            '<Proxy *>\n',
+            '    Order deny,allow\n',
+            '    Allow from all\n',
+            '</Proxy>\n'
+        ];
+        proxy.forEach(function(item) {
+            strs.push('ProxyPass ' + item.dir + ' ' + item.path + '\n');
+            strs.push('ProxyPassReverse ' + item.dir + ' ' + item.path + '\n');
+        });
+        return strs.join('');
+    },
+    //创建虚拟主机
+    createVhost: function(option) {
+        var template = [
+            '\n##############################\n#' + (option.comment || option.name) + '\n##############################\n',
+            '<VirtualHost *:80>\n',
+            '    ServerAdmin ' + (option.admin || 'admin@admin.com') + '\n',
+            '    DocumentRoot "' + option.root + '"\n',
+            '    ServerName ' + option.name + '\n',
+            '    <Directory "' + option.root + '">\n',
+            '        Options Indexes FollowSymLinks\n',
+            '        AllowOverride All\n',
+            '        Order allow,deny\n',
+            '        Allow from all\n',
+            '    </Directory>\n'
+        ];
+        if(option.proxy) {
+            template.push(this.createProxy(option.proxy));
+        }
+        template.push('</VirtualHost>');
+        var data = template.join('');
+        fs.writeFileSync(this.path, data, {
+            flag: 'a'
+        });
+        this.readFileSync();
+        return data;
+    }
+};
+
+/*module.exports = {
+    platform: Platform,
+    apache: Apache,
+    httpd: Httpd,
+    host: Host
+};*/
---
+++ nw/js/index.js
@@ -0,0 +1,320 @@
+var httpd = new Httpd();
+httpd.init();
+var host = new Host();
+var apache = new Apache();
+var qs = require('qs');
+
+var Dialog = (function() {
+    var template = $('#J_tmpl_modal_dialog').html();
+    return {
+        alert: function(msg) {
+            var html = Mustache.render(template, {
+                title: '提示 ',
+                body: msg,
+                ok: '确定'
+            });
+            var $html = $(html);
+            $html.modal();
+            $html.find('.btn-primary').on('click', function() {
+                $html.modal('hide');
+            })
+        },
+        confirm: function() {
+
+        },
+
+    }
+}());
+
+//获取IP
+var network = os.networkInterfaces();
+var en0 = network.en0;
+if(!en0) {
+    var arr = [];
+    for(var i in network) {
+        arr.push(network[i]);
+    }
+    en0 = arr[0];
+}
+en0.forEach(function(item, i) {
+    if(item.family === 'IPv4') {
+        $('#J_ip').val(item.address);
+    }
+})
+
+
+$('#J_ip').on('mouseenter', function() {
+    $(this).select();
+});
+
+//获取vhost列表
+function loadHttpd() {
+    var conf = httpd.getData();
+    var objs = [];
+    var list = httpd.getList(conf);
+    if(list && list.length > 0) {
+        list.forEach(function(item) {
+            var obj = httpd.getObj(item);
+            objs.push(obj);
+        });
+        var json = {
+            success: true,
+            data: objs
+        };
+        json._proxy = function() {
+            return JSON.stringify(this.proxy);
+        };
+        var html = Mustache.render($('#J_tmpl_vhost_item').html(), json);
+        $('#J_vhost_list').html(html);    
+    }
+}
+loadHttpd();
+
+if(localStorage.getItem('env-vhost')) {
+    $('#J_vhost').val(localStorage.getItem('env-vhost'));
+}
+//读取vhost文本
+$('#J_btn_read_vhost').on('click', function() {
+    httpd.readFile(function(err, data) {
+        if(err) {
+            
+        }
+        $('#J_vhost').val(data).css('color', '#080');;
+        localStorage.setItem('env-vhost', data);
+    });
+});
+//写入vhost文本
+$('#J_btn_write_vhost').on('click', function() {
+    var data = $('#J_vhost').val()
+    httpd.writeFile(data, function(err) {
+        if(err) {
+            
+        }
+        Dialog.alert('写入成功');
+        localStorage.setItem('env-vhost', data);
+    });
+});
+
+//添加
+$('#J_vhost_add').on('click', function() {
+    var dialogTmpl = $('#J_tmpl_modal_dialog').html();
+    var dialog = Mustache.render(dialogTmpl, {
+        title: '添加vhost',
+        body: Mustache.render($('#J_tmpl_vhost_form').html()),
+        close: '关闭',
+        ok: '提交'
+    }/*, {
+        body: Mustache.render($('#J_tmpl_vhost_form').html())
+    }*/);
+    var $dialog = $(dialog);
+    $dialog.modal();
+    setTimeout(function() {
+        $dialog.find('.form-control').eq(0).focus();
+    }, 500);
+    
+    $dialog.on('click', '.J_delete', function(e) {
+        e.preventDefault();
+        var $li = $(this).closest('li');
+        $li.remove();
+    });
+    $dialog.on('click', '.J_add', function(e) {
+        var $html = $(Mustache.render($('#J_tmpl_proxy_item').html()));
+        $dialog.find('.J_proxy_list').append($html);
+        $html.find('.form-control').eq(0).focus();
+    });
+    $dialog.on('click', '.J_submit', function() {
+        var $form = $dialog.find('form');
+        var param = $form.serialize();
+        var body = qs.parse(param);
+        var name = body.name;
+        var root = body.root;
+        var proxyDir = body.proxyDir;
+        var proxyPath = body.proxyPath;
+        if(!name) {
+            alert('name不能为空');
+        }
+        var item = httpd.getItem(name);
+        //console.log(item);
+        if(item) {
+            alert('该配置已存在');
+        }
+        if(!root) {
+            alert('root不能为空');
+        }
+        var obj = {
+            name: name,
+            root: root
+        };
+        //如果有配置代理
+        if(proxyDir && proxyPath) {
+            var proxyArr = [];
+            if(typeof proxyDir === 'string') {
+                proxyArr.push({
+                    dir: proxyDir,
+                    path: proxyPath
+                });
+            }else if(typeof proxyDir === 'object') {
+                proxyDir.forEach(function(item, i) {
+                    proxyArr.push({
+                        dir: item,
+                        path: proxyPath[i]
+                    });
+                })
+            }
+            obj.proxy = proxyArr;
+        }
+        var result = httpd.createVhost(obj);
+        var json = {
+            success: true,
+            data: httpd.getObj(result)
+        };
+        $dialog.modal('hide');
+        Dialog.alert('添加成功');
+        json._proxy = function() {
+            return JSON.stringify(this.proxy);
+        };
+        var html = Mustache.render($('#J_tmpl_vhost_item').html(), json);
+        $('#J_vhost_list').append(html);
+    });
+});
+
+//编辑
+$('#J_vhost_list').on('click', '.J_edit', function(e) {
+    e.preventDefault();
+    var $tr = $(this).closest('tr');
+    var name = $tr.attr('data-name');
+    var root = $tr.attr('data-root');
+    var proxy = JSON.parse($tr.attr('data-proxy'));
+    var dialogTmpl = $('#J_tmpl_modal_dialog').html();
+    var dialog = Mustache.render(dialogTmpl, {
+        title: '更新',
+        body: Mustache.render($('#J_tmpl_vhost_form').html(), {
+            name: name,
+            root: root,
+            proxys: proxy.list
+        }, {
+            body: $('#J_tmpl_proxy_item').html()
+        }),
+        close: '关闭',
+        ok: '提交'
+    }/*, {
+        body: Mustache.render($('#J_tmpl_vhost_form').html(), {
+            name: name,
+            root: root,
+            proxys: proxy.list
+        }, {
+            body: $('#J_tmpl_proxy_item').html()
+        })
+    }*/);
+    var $dialog = $(dialog);
+    $dialog.modal();
+    $dialog.on('click', '.J_delete', function(e) {
+        e.preventDefault();
+        var $li = $(this).closest('li');
+        $li.remove();
+    });
+    $dialog.on('click', '.J_add', function(e) {
+        var html = Mustache.render($('#J_tmpl_proxy_item').html());
+        $dialog.find('.J_proxy_list').append(html);
+    });
+    $dialog.on('click', '.J_submit', function() {
+        var $form = $dialog.find('form');
+        var param = $form.serialize();
+        var body = qs.parse(param);
+        var newName = body.name;
+        var root = body.root;
+        var proxyDir = body.proxyDir;
+        var proxyPath = body.proxyPath;
+        var proxy = null;
+        //如果有配置代理
+        if(proxyDir && proxyPath) {
+            var proxyArr = [];
+            if(typeof proxyDir === 'string') {
+                proxyArr.push({
+                    dir: proxyDir,
+                    path: proxyPath
+                });
+            }else if(typeof proxyDir === 'object') {
+                proxyDir.forEach(function(item, i) {
+                    proxyArr.push({
+                        dir: item,
+                        path: proxyPath[i]
+                    });
+                })
+            }
+            proxy = proxyArr;
+        }
+        var obj = {
+            name: newName,
+            root: root,
+            proxy: proxy || ''
+        };
+        httpd.updateItem(name, obj, function(err, data) {
+            var json = {
+                success: !err,
+                data: httpd.getObj(data)
+            };
+            if(err) {
+                return Dialog.alert('更新失败');
+            } else {
+                $dialog.modal('hide');
+                Dialog.alert('更新成功');
+                json._proxy = function() {
+                    return JSON.stringify(this.proxy);
+                };
+                var html = Mustache.render($('#J_tmpl_vhost_item').html(), json);
+                $tr.replaceWith(html);
+            }
+        });
+    });
+});
+//删除
+$('#J_vhost_list').on('click', '.J_delete', function(e) {
+    e.preventDefault();
+    var $tr = $(this).closest('tr');
+    var name = $tr.attr('data-name');
+    var result = httpd.removeItem(name);
+    Dialog.alert('删除成功');
+    $tr.remove();
+});
+//重启apache
+$('#J_restart').on('click', function() {
+    console.log('重启');
+    var $btn = $(this);
+    $btn.button('loading');
+    apache.restart(function(err, stdout, stderr) {
+        //console.log(err, stdout, stderr);
+        if(err) {
+            return console.log('err');
+        }
+        $btn.button('reset');
+        Dialog.alert('重启成功');
+    });
+});
+
+
+//host
+if(localStorage.getItem('env-host')) {
+    $('#J_host').val(localStorage.getItem('env-host'));
+}
+$('#J_read').on('click', function() {
+    var $btn = $(this);
+    $btn.button('loading');
+    
+    host.read(function(err, data) {
+        $btn.button('reset');
+        $('#J_host').val(data).css('color', '#080');
+        localStorage.setItem('env-host', data);
+    })
+});
+$('#J_write').on('click', function() {
+    var content = $('#J_host').val();
+    host.write(content, function(err) {
+        if(err) {
+            return Dialog.alert('写入失败');
+        } else {
+            localStorage.setItem('env-host', content);
+            Dialog.alert('写入成功');
+        }
+    })
+});
