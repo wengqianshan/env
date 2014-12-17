b20dbcab1d4b877a899c5a742e395ed11b171dbb
弱冠 <qianshan.wengqs@alibaba-inc.com>
2014-12-17 15:59:25 +0800

    注释修改

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
         vhost: ''//httpd-vhosts.conf文件路径，比如windows下 D:/AppServ/Apache2.2/conf/extra/httpd-vhosts.conf
     },
     dns: {
-        nameserver: '8.8.8.8'//备用dns解析服务,alibaba-inc网络下一般为：10.65.0.1
+        nameserver: '8.8.8.8'//备用dns解析服务. 10.65.0.1
     },
     combo: {
         dir: '/Users/xiaoshan/gitlab/ilw',//
